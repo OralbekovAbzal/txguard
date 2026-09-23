06e8301b24dac7a009d39483f80ba5aaba82a932
OralbekovAbzal <abzalprofile@gmail.com>
2026-09-23 21:52:58 +0500

    Add city rule with fail-safe

--- main.py
+++ main.py
@@ -5,16 +5,50 @@ transactions = [
     {"id": 1, "client": "Abzal", "amount": 50000, "city": "Astana"},
     {"id": 2, "client": "Ansar", "amount": 346000, "city": "Astana"},
     {"id": 3, "client": "Assel", "amount": 79000, "city": "Pavlodar"},
-    {"id": 4, "client": "Baurzhan", "amount": 70000, "city": "Oskemen"},
-    {"id": 5, "client": "Abzal", "amount": 700000, "city": "Astana"}
+    {"id": 4, "client": "Unknown", "amount": 70000, "city": None},
+    {"id": 5, "client": "Abzal", "amount": 700000, "city": "Astana"},
+    {"id": 6, "client": "Baurzhan", "amount": 70000, "city": "Oskemen"}
     ]
 
-def check_amount(tr: dict) -> str:
-    if tr["amount"] > BLOCK_LIMIT:
+home_cities = {
+    "Abzal" : "Astana",
+    "Ansar" : "Astana",
+    "Assel" : "Astana",
+    "Baurzhan" : "Oskemen"
+}
+
+def check_amount(tx: dict) -> str:
+    #Если сумма превышает лимит по транзакциям то блокируется
+    if tx["amount"] > BLOCK_LIMIT:
         return "block"
-    elif tr["amount"] > REVIEW_LIMIT:
+    #Если сумма превышает лимит то идет на проверку
+    elif tx["amount"] > REVIEW_LIMIT:
         return "review"
     return "approve"
 
-for t in transactions:
-    print(f'Платеж {t["id"]} ({t["client"]}, {t["amount"]}): {check_amount(t)}')
+def check_city(tx: dict) -> str:
+    home = home_cities.get(tx["client"])
+    #Если у транзакции нету города то идет на проверку
+    if home is None:
+        return "review"
+
+    #Если город проживания и город в котором была сделана транзакция разная то идет на проверку
+    if tx["city"] != home:
+        return "review"
+    
+    return "approve"
+
+#Оценка транзакции по итогам двух проверок
+def score(tx: dict) -> str:
+    results = []
+    results.append(check_amount(tx))
+    results.append(check_city(tx))
+    if "block" in results:
+        return "block"
+    elif "review" in results:
+        return "review"
+    else:
+        return "approve"
+
+for tx in transactions:
+    print(f'Платеж {tx["id"]} ({tx["client"]}, {tx["amount"]}): {score(tx)}')
